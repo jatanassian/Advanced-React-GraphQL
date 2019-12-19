1b06f9c275f138d49a3b5fdae239fbd6a578ee4c
Julien Atanassian <julien.atanassian@gmail.com>
2019-12-19 18:14:25 -0500

    import styled-components and create a StyledPage component with the general style of the page

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 
 import Header from '../components/Header';
 import Meta from '../components/Meta';
 
+const StyledPage = styled.div`
+  background: white;
+  color: black;
+`;
+
 export default class Page extends Component {
   render() {
     return (
-      <div>
+      <StyledPage>
         <Meta />
         <Header />
         {this.props.children}
-      </div>
+      </StyledPage>
     )
   }
 }
