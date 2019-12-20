0c082513ec3a6bb8607464c2dae6e24c09a5ce66
Julien Atanassian <julien.atanassian@gmail.com>
2019-12-20 18:49:11 -0500

    add style to the anchor link inside the Logo component

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -8,6 +8,13 @@ const Logo = styled.h1`
   position: relative;
   z-index: 2;
   transform: skew(-7deg);
+  a {
+    padding: 0.5rem 1rem;
+    background: ${props => props.theme.red};
+    color: white;
+    text-transform: uppercase;
+    text-decoration: none;
+  }
 `;
 
 const Header = () => (
