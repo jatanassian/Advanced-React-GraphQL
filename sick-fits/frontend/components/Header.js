92d6da64cd0ec0ac1ab661430e750308c772f353
Julien Atanassian <julien.atanassian@gmail.com>
2019-12-20 19:03:03 -0500

    add a media query

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -28,6 +28,10 @@ const StyledHeader = styled.header`
     grid-template-columns: auto 1fr;
     justify-content: space-between;
     align-items: stretch;
+    @media (max-width: 1300px) {
+      grid-template-columns: 1fr;
+      justify-content: center;
+    }
   }
 `;
 
