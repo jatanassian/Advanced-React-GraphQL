a614feb19022db44bd00c5395f082c71b320538b
Julien Atanassian <julien.atanassian@gmail.com>
2020-01-09 18:06:10 -0500

    use the createServer function to start a version of the yoga server

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,20 @@
-// let's go!
+require('dotenv').config({ path: 'variables.env' });
+const createServer = require('createServer');
+const db = require('./db');
+
+const server = createServer();
+
+// TODO Use express middleware to handle cookies (JWT)
+// TODO Use express middleware to populate current user
+
+server.start(
+  {
+    // Make sure that the endpoint can only be used by our approved URLs
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    }
+  }, deets => {
+    console.log(`Server is now running on port http://localhost:${process.env.PORT}`)
+  }
+);
