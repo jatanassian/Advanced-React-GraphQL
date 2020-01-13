c26024dcbcb11e6b22a2ce0cb522c3b5ab673777
Julien Atanassian <julien.atanassian@gmail.com>
2020-01-13 16:17:16 -0500

    add an other method for the same resolver as the query is the same as the one in prisma, we can just forward that query from yoga to prisma

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,9 +1,16 @@
-const Query = {
-  async items(parents, args, ctx, info) {
-    const items = await ctx.db.query.items();
+// If the query is exactly the same on Prisma and your query (can happen when there's no custom logic such as authentication or filtering), you can jsut forward that query from Yoga to Prisma instead of writing the full query (see query commented below).
+const { forwardTo } = require('prisma-binding');
 
-    return items;
-  }
+const Query = {
+  items: forwardTo('db')
 };
+
+// const Query = {
+//   async items(parents, args, ctx, info) {
+//     const items = await ctx.db.query.items();
+
+//     return items;
+//   }
+// };
 
 module.exports = Query;
